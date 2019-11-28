61a15906d9a3379efdd490a2670bf661e14170ed
Muhammad Asad <muhammadasadahsan@gmail.com>
2019-11-28 20:27:50 +0500

    class work week 9

---
+++ app/src/main/java/com/example/muhammadasad/fall19/SharedPrefenceManager.java
@@ -0,0 +1,12 @@
+package com.example.muhammadasad.fall19;
+
+import android.content.SharedPreferences;
+
+public class SharedPrefenceManager {
+
+    private SharedPrefenceManager(){
+
+    }
+
+
+}
